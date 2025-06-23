e88c72b1b5cd865fa955fbeccdf5298dba9c0830
ErikaSignal <erika.signal@gmail.com>
2025-06-23 19:58:10 +0200

    Update code

--- src/main.js
+++ src/main.js
@@ -9,15 +9,16 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 300,
+    height: 400,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'startPage.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
